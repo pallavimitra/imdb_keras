8c856a7f1db6571594af12bf7d655e7092954729
PALLAVI MITRA <pallavi.mitra1992@gmail.com>
2020-05-13 22:10:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def run():
     try:
-        #data = request.get_json(force=True)
+        data = request.get_json(force=True)
         #input_params = data['input']
         #result =  predict.predict(input_params)
         #return jsonify({'prediction': result})
-        return make_response(jsonify({'Hello world!!'}))
+        return jsonify({'data': data})
     except Exception as e:
         print(traceback.format_exc())
         return jsonify({'error': str(e)})
