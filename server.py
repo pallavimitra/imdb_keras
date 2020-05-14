55880da4a721af855ab7e41948dfe52bce970a62
PALLAVI MITRA <pallavi.mitra1992@gmail.com>
2020-05-14 16:29:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,30 +1,22 @@
 from flask import Flask, jsonify, make_response, request
 import traceback 
 import predict
-import sys
+
 
 app = Flask(__name__)
 
-'''@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def run():
     try:
         #data = request.get_json(force=True)
-        #print({'data recived' : data})
         #input_params = data['input']
-        #print({'input recived' : input_params})
         #result =  predict.predict(input_params)
-        #print('prediction')
-        #print(result)
         #return jsonify({'prediction': result})
         return "Hello World!!"
     except Exception as e:
         print(traceback.format_exc())
         return jsonify({'error': str(e)})'''
 
-@app.route('/score', methods=['POST'])
-def score():
-    features = request.json['X']
-    return make_response(jsonify({'score': features}))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
